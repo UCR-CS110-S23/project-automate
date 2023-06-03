d8041884887335efc47880a7d7385301dcaf68b5
Jacob <jramo146@ucr.edu>
2023-06-03 16:23:51 -0700

    Configured CORS, connected to DB, Setup Session, Authentication, etc.

--- back/index.js
+++ back/index.js
@@ -10,12 +10,13 @@ const bodyParser = require( 'body-parser');
 
 const app = express(); 
 const server = http.createServer(app);
+const io = socketIO(server);
 
-
+const routes = require('./routes/auth');
+const rooms = require('./routes/rooms');
 
 // TODO: add cors to allow cross origin requests
-
-
+app.use(cors());
 
 
 dotenv.config();
@@ -23,21 +24,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-
 // Connect to the database
 // TODO: your code here
+mongoose.connect(process.env.mongodb);
+const database = mongoose.connection;
 
+database.once('connected', ()=>{
+  console.log("Connected to the DB!");
+})
 
 
 // Set up the session
 // TODO: your code here
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+}))
 
-
-
-
-const routes = require('./routes/auth');
-const rooms = require('./routes/rooms');
-
+//app.use('/api/auth', auth);
+//app.use('/api/posts',posts);
 
 app.get('/', (req, res) => {
   if (req.session && req.session.authenticated) {
@@ -53,7 +59,7 @@ app.get('/', (req, res) => {
 app.use("/api/auth/", routes);
 
 
-// checking the session before accessing the rooms
+// check session before access to rooms
 app.use((req, res, next) => {
   if (req.session && req.session.authenticated) {
     next();
@@ -63,8 +69,6 @@ app.use((req, res, next) => {
 });
 app.use("/api/rooms/", rooms);
 
-
-
 // Start the server
 server.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
@@ -73,11 +77,43 @@ server.listen(process.env.PORT, () => {
 
 // TODO: make sure that the user is logged in before connecting to the socket
 // TODO: your code here
+io.use((socket, next) => {
+  // Access socket session
+  const session = socket.request.session;
 
+  if (session && session.authenticated) {
+    // user logged in and authenticated
+    next();
+  } else {
+    // User not logged in
+    next(new Error('Unauthorized'));
+  }
+});
 
+io.on('connection', (socket) => {
+  console.log('user connected');
+  // Handle events for logged in user
+  socket.on('chatMessage', (message) => {
+    // If user is logged in handle chat events
+    if (socket.request.session && socket.request.session.authenticated) { 
+      console.log(`Message received from authenticated user: ${message}`);
 
-io.on('connection', (socket)=>{
-  console.log("user connected")
-  // TODO: write codes for the messaging functionality
-  // TODO: your code here
+      // Broadcast to additional connected clients
+      socket.broadcast.emit('chatMessage', message);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 })
+
+/*
+io.on('connection', (socket)=>{
+    console.log("user connected")
+    // TODO: write codes for the messaging functionality
+    // TODO: your code here
+})
+*/
+
+
